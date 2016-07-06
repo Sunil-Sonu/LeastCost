7083021ef5bce62b9f9ba47fcde508bf3af5afae
Sunil-Sonu <Sunil-Sonu@users.noreply.github.com>
2016-07-07 02:52:18 +0530

    Update LeastCost.c

--- LeastCost.c
+++ LeastCost.c
@@ -9,8 +9,6 @@ void leastSum(struct node *root, int sum, int *min)
 		leastSum(root->left,sum+=root->data,min);
 		if ((sum < *min || *max==0) && (root->left == NULL && root->right == NULL)) //Condition to check if it's leaf with minimum value.
 			*min = sum;
-		else
-			sum = sum - root->data;
 		leastSum(root->right, sum += root->data, min);
 	}
 }
